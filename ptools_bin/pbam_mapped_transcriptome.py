dd987d01156ce3c2bb79c16d777b7132aca68f84
gursoygamze <41677943+gursoygamze@users.noreply.github.com>
2020-11-23 07:05:32 -0500

    Update pbam_mapped_transcriptome.py

--- ptools_bin/pbam_mapped_transcriptome.py
+++ ptools_bin/pbam_mapped_transcriptome.py
@@ -1,5 +1,5 @@
 ## Gamze Gursoy ##
-##last edit June 7th, 2020
+##last edit Nov 23rd, 2020
 ## input arguments
 ## (1) reference transcriptome
 ## (2) reference genome
@@ -71,6 +71,8 @@ def main():
                     pbam[i] = "NM:i:0"
                 if t[0] == "nM":
                     pbam[i] = "nM:i:0"
+                if t[0] == "MC":
+                    pbam[i] = "MC:Z:" + str(RL) + "M"
             chrom = str(pbam[2])
             startPos = int(pbam[3])
             k = ref.query(chrom, startPos - 1, int(RL))
